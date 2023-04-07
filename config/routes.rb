a7675e396836ccb4f2badcf197195503d5338b1f
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-04-07 15:48:56 +0000

    [create]twitterクローン

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  get 'relationships/create'
+  get 'relationships/destroy'
+
+  root 'tweets#index'
+  devise_for :users
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
